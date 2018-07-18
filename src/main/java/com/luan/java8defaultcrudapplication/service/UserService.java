6dd0980474641e203f05c0b80bc938fbda9dedbe
Luan Sousa <luan26@uninove.edu.br>
2018-07-18 20:34:26 -0300

    Service created

---
+++ src/main/java/com/luan/java8defaultcrudapplication/service/UserService.java
@@ -0,0 +1,13 @@
+package com.luan.java8defaultcrudapplication.service;
+import java.util.List;
+
+import com.luan.java8defaultcrudapplication.service.dto.UserDTO;
+
+public interface UserService {
+
+	UserDTO save (UserDTO userDTO);
+	
+	List<UserDTO> findAll();
+	
+	UserDTO findOne(Long idUser);
+}
