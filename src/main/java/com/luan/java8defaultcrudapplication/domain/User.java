33c8110add7bd76e114aa207b32b0b08b66d72a7
Luan Sousa <luan26@uninove.edu.br>
2018-07-18 20:21:27 -0300

    correção no domain

--- src/main/java/com/luan/java8defaultcrudapplication/domain/User.java
+++ src/main/java/com/luan/java8defaultcrudapplication/domain/User.java
@@ -1,9 +1,28 @@
 package com.luan.java8defaultcrudapplication.domain;
 
-public class User {
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
 
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_USER")
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private Long id;
+	
+	@Column(name="USER_NAME", nullable = false )
 	private String name;
+	
+	@Column(name="USER_JOB", nullable = false)
 	private String job;
+	
+	@Column(name="USER_AGE", nullable = false)
 	private int age;
 	
 	public void setName(String name) {
