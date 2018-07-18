0f4cfd2f03b99a951f090c3f7502f89acf89199f
Luan Sousa <luan26@uninove.edu.br>
2018-07-17 23:04:56 -0300

    first commit of project

---
+++ src/main/java/com/luan/default/java8defaultcrudapplication/Java8DefaultCrudApplication.java
@@ -0,0 +1,12 @@
+package com.luan.default.java8defaultcrudapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8DefaultCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8DefaultCrudApplication.class, args);
+	}
+}
