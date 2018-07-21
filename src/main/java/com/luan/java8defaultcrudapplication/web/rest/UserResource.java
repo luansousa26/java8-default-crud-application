f68c60f92fe20dc83c85d11a05076bc7b6fc825a
Luan Sousa <luan26@uninove.edu.br>
2018-07-21 09:17:55 -0300

    Post criado

--- src/main/java/com/luan/java8defaultcrudapplication/web/rest/UserResource.java
+++ src/main/java/com/luan/java8defaultcrudapplication/web/rest/UserResource.java
@@ -1,7 +1,26 @@
 package com.luan.java8defaultcrudapplication.web.rest;
+import java.net.URISyntaxException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class UserResource {
+import com.luan.java8defaultcrudapplication.service.UserService;
+import com.luan.java8defaultcrudapplication.service.dto.UserDTO;
 
+@RestController
+@RequestMapping("/users")
+public class UserResource {
+	
+	private final UserService userService;
+    
+	private UserResource(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@PostMapping()
+	public ResponseEntity<UserDTO> save(@RequestBody UserDTO userDTO) throws URISyntaxException {
+		return ResponseEntity.ok().body(userService.save(userDTO));
+	}
 }
