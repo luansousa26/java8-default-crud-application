f54e3c4e04b378a99df65cc48c7ca2385b0d09c8
Luan Sousa <luan26@uninove.edu.br>
2018-07-21 09:27:08 -0300

    FindAll and findByID created

--- src/main/java/com/luan/java8defaultcrudapplication/web/rest/UserResource.java
+++ src/main/java/com/luan/java8defaultcrudapplication/web/rest/UserResource.java
@@ -1,6 +1,10 @@
 package com.luan.java8defaultcrudapplication.web.rest;
 import java.net.URISyntaxException;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,6 +31,17 @@ public class UserResource {
 	
 	@PutMapping()
 	public ResponseEntity<UserDTO> update(@RequestBody UserDTO userDTO) throws URISyntaxException {
+		if(userDTO.getId() == null) {
+			return save(userDTO);
+		}
 		return ResponseEntity.ok().body(userService.update(userDTO));
+	}
+	@GetMapping()
+	public ResponseEntity<List<UserDTO>> getAll() throws URISyntaxException{
+		return ResponseEntity.ok().body(userService.findAll());
+	}
+	@GetMapping("/findByID{idUser}")
+	public ResponseEntity<UserDTO> getByID(@PathVariable("idUser") Long idUser ) throws URISyntaxException{
+		return ResponseEntity.ok().body(userService.findOne(idUser));
 	}
 }
