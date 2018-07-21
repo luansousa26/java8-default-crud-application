30cfb6f2cb3f32b2cccb91080105a77272409b2d
Luan Sousa <luan26@uninove.edu.br>
2018-07-21 10:08:22 -0300

    spring web starter added

--- src/main/java/com/luan/java8defaultcrudapplication/Java8DefaultCrudApplication.java
+++ src/main/java/com/luan/java8defaultcrudapplication/Java8DefaultCrudApplication.java
@@ -1,12 +1,17 @@
 package com.luan.java8defaultcrudapplication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Java8DefaultCrudApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(Java8DefaultCrudApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Java8DefaultCrudApplication.class, args);
+		log.info("PROJECT LUAN RUNNING");
 	}
 }
