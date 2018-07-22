e4316fbde8bf3c9a0e7f77b2d095d3a9403dcc5d
Luan Sousa <luan26@uninove.edu.br>
2018-07-21 23:28:16 -0300

    Apimodel added

--- src/main/java/com/luan/java8defaultcrudapplication/service/dto/UserDTO.java
+++ src/main/java/com/luan/java8defaultcrudapplication/service/dto/UserDTO.java
@@ -2,16 +2,24 @@ package com.luan.java8defaultcrudapplication.service.dto;
 
 import java.io.Serializable;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(value = "Users")
 public class UserDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@ApiModelProperty(value = "User Identify.")
     private Long id;
-	
+
+	@ApiModelProperty(value = "User name.")
 	private String name;
 	
+	@ApiModelProperty(value = "User job.")
 	private String job;
 
+	@ApiModelProperty(value= " User age.")
 	private int age;
 	
 	public Long getId() {
