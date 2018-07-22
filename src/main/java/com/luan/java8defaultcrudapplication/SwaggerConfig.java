3e1f779ed4fdec2e7b5c1c8a8d78a5398859d16a
Luan Sousa <luan26@uninove.edu.br>
2018-07-22 00:04:04 -0300

    Swagger running

--- src/main/java/com/luan/java8defaultcrudapplication/SwaggerConfig.java
+++ src/main/java/com/luan/java8defaultcrudapplication/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.luan.java8defaultcrudapplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -7,6 +9,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
+@Configuration
 public class SwaggerConfig {
 	@Bean
 		public Docket detalheApi() {
