3d5b413ad5d56469fb735e3c69dfac7bda20175e
Luan Sousa <luan26@uninove.edu.br>
2018-07-25 20:17:29 -0300

    sequecy added for create ids

--- src/main/java/com/luan/java8defaultcrudapplication/domain/User.java
+++ src/main/java/com/luan/java8defaultcrudapplication/domain/User.java
@@ -5,16 +5,19 @@ import java.time.LocalDate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "TB_USER")
+@SequenceGenerator(name="sequency", initialValue=1, allocationSize=100)
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="sequency")
 	@Id
 	private Long id;
 	
