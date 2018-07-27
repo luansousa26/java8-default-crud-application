f7da754d6894c4bb97ecbe8a4b884bbd8884ce1b
Luan Sousa <luan26@uninove.edu.br>
2018-07-27 09:37:02 -0300

    cnpj getter and setter added

--- src/main/java/com/luan/java8defaultcrudapplication/utils/DocumentValidator.java
+++ src/main/java/com/luan/java8defaultcrudapplication/utils/DocumentValidator.java
@@ -13,4 +13,12 @@ public class DocumentValidator {
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+	
+	public String getCnpj() {
+		return this.cnpj;
+	}
+	
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
 }
