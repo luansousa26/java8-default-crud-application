96601de07e4be3a15fc03d1cd62fd00cec445e50
Luan Sousa <luan26@uninove.edu.br>
2018-07-27 09:55:51 -0300

    Alterartion

--- src/main/java/com/luan/java8defaultcrudapplication/utils/Address.java
+++ src/main/java/com/luan/java8defaultcrudapplication/utils/Address.java
@@ -1,5 +1,11 @@
 package com.luan.java8defaultcrudapplication.utils;
 
 public class Address {
-
+     
+	private String street;
+	private String district;
+	private Long number;
+	private String city;
+	private String state;
+	private String cep;
 }
