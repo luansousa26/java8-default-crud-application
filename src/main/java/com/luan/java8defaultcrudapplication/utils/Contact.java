e3f32d7c1f43772df260c828c9a37f1aac740517
Luan Sousa <luan26@uninove.edu.br>
2018-07-27 10:12:11 -0300

    Contact implemented

--- src/main/java/com/luan/java8defaultcrudapplication/utils/Contact.java
+++ src/main/java/com/luan/java8defaultcrudapplication/utils/Contact.java
@@ -1,5 +1,50 @@
 package com.luan.java8defaultcrudapplication.utils;
 
 public class Contact {
-
+	
+	private String phone;
+	private String cellPhone;
+	private String commercialPhone;
+	private String email;
+	private String pager;
+	
+	public String getPhone() {
+		return this.phone;
+	}
+	
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public String getCellPhone() {
+		return this.cellPhone;
+	}
+	
+	public void setCellPhone(String cellPhone) {
+		this.cellPhone = cellPhone;
+	}
+	
+	public String getCommercialPhone() {
+		return this.commercialPhone;
+	}
+	
+	public void setCommercialPhone(String commercialPhone) {
+		this.commercialPhone = commercialPhone;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getPager() {
+		return this.pager;
+	}
+	
+	public void setPager(String pager) {
+		this.pager = pager;
+	}
 }
