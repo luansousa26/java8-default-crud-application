9c84a497377ea330889478575e076fada3c25e01
Luan Sousa <luan26@uninove.edu.br>
2018-07-27 10:05:37 -0300

    Alteration

--- src/main/java/com/luan/java8defaultcrudapplication/utils/Address.java
+++ src/main/java/com/luan/java8defaultcrudapplication/utils/Address.java
@@ -8,4 +8,52 @@ public class Address {
 	private String city;
 	private String state;
 	private String cep;
+	
+	public String getStreet() {
+		return this.street;
+	}
+	
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	
+	public String getDistrict() {
+		return this.district;
+	}
+	
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+	
+	public Long getNumber() {
+		return this.number;
+	}
+	
+	public void setNumber(Long number) {
+		this.number = number;
+	}
+	
+	public String getCity() {
+		return this.city;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public String getState() {
+		return this.state;
+	}
+	
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	public String getCep() {
+		return this.cep;
+	}
+	
+	public void setCep(String cep) {
+		this.cep  = cep;
+	}
 }
