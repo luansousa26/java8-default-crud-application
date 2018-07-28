aa8026d7170c39c33db8d20d27d0a52faf87df11
Luan Sousa <luan26@uninove.edu.br>
2018-07-28 12:20:43 -0300

    contact resource added

---
+++ src/main/java/com/luan/java8defaultcrudapplication/web/rest/ContactResource.java
@@ -0,0 +1,11 @@
+package com.luan.java8defaultcrudapplication.web.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+
+@RestController
+@Api("/contact/")
+public class ContactResource {
+
+}
