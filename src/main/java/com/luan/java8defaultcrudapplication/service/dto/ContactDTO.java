1b84b34d727f5847908994aa33ce9114f502ce7d
Luan Sousa <luan26@uninove.edu.br>
2018-07-28 12:23:39 -0300

    Contact DTO created

---
+++ src/main/java/com/luan/java8defaultcrudapplication/service/dto/ContactDTO.java
@@ -0,0 +1,9 @@
+package com.luan.java8defaultcrudapplication.service.dto;
+
+import java.io.Serializable;
+
+public class ContactDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
