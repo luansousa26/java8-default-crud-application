2b35f5a470825115525a8a1f4691f81b427844d1
Luan Sousa <luan26@uninove.edu.br>
2018-07-28 14:09:46 -0300

    Swagger implemented in ContactDTO

--- src/main/java/com/luan/java8defaultcrudapplication/service/dto/ContactDTO.java
+++ src/main/java/com/luan/java8defaultcrudapplication/service/dto/ContactDTO.java
@@ -4,23 +4,34 @@ import java.time.LocalDate;
 
 import org.springframework.stereotype.Component;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 @Component
+@ApiModel(value = "contacts")
 public class ContactDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@ApiModelProperty(value = "Client Identifier Contact.")
 	private Long id;
 	
+	@ApiModelProperty(value = "Client Phone Contact.")
 	private String phone;
 
+	@ApiModelProperty(value = "Client CellPhone Contact.")
 	private String cellPhone;
 	
+	@ApiModelProperty(value = "Client Commercial phone Contact.")
 	private String commercialPhone;
 	
+	@ApiModelProperty(value = "Client email Contact.")
 	private String email;
 	
+	@ApiModelProperty(value = "Client pager Contact.")
 	private String pager;
 	
+	@ApiModelProperty(value = "Client id.")
 	private Long idClient;
 	
 	private LocalDate alterationDate;
