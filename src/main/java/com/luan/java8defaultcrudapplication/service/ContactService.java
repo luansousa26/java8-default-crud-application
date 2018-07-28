666c6acd4e3d2ca690c47009adbbff4829902793
Luan Sousa <luan26@uninove.edu.br>
2018-07-28 12:27:09 -0300

    Contact Service Implemented

--- src/main/java/com/luan/java8defaultcrudapplication/service/ContactService.java
+++ src/main/java/com/luan/java8defaultcrudapplication/service/ContactService.java
@@ -1,5 +1,19 @@
 package com.luan.java8defaultcrudapplication.service;
 
-public interface ContactService {
+import java.util.List;
+import java.util.Optional;
 
+import com.luan.java8defaultcrudapplication.service.dto.ContactDTO;
+
+public interface ContactService {
+   
+	ContactDTO save(ContactDTO contactDTO);
+	
+	List<ContactDTO> findAll();
+	
+	ContactDTO update(ContactDTO contactDTO);
+	
+	void delete(Long idContact);
+	
+	Optional<ContactDTO> findById(Long IdContact);
 }
