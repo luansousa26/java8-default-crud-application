28662376b766a0b8c360fea88b480fce3ae73c46
Luan Sousa <luan26@uninove.edu.br>
2018-07-28 12:35:54 -0300

    put mapping implemented

--- src/main/java/com/luan/java8defaultcrudapplication/web/rest/ContactResource.java
+++ src/main/java/com/luan/java8defaultcrudapplication/web/rest/ContactResource.java
@@ -3,6 +3,7 @@ import java.net.URISyntaxException;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.luan.java8defaultcrudapplication.service.ContactService;
@@ -24,4 +25,10 @@ private final ContactService contactService;
 	public ResponseEntity<ContactDTO> save(@RequestBody ContactDTO contactDTO) throws URISyntaxException {
 		return ResponseEntity.ok().body(contactService.save(contactDTO));
 	}
+	
+	@PutMapping()
+	public ResponseEntity<ContactDTO> update(@RequestBody ContactDTO contactDTO) throws URISyntaxException {
+		return ResponseEntity.ok().body(contactService.update(contactDTO));
+	}
+	
 }
