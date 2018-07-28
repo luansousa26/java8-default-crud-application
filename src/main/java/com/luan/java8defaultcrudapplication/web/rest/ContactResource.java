72eb3083ab3af590c4a3ea5b47f989ef533e4215
Luan Sousa <luan26@uninove.edu.br>
2018-07-28 12:32:14 -0300

    Post mapping created

--- src/main/java/com/luan/java8defaultcrudapplication/web/rest/ContactResource.java
+++ src/main/java/com/luan/java8defaultcrudapplication/web/rest/ContactResource.java
@@ -1,11 +1,27 @@
 package com.luan.java8defaultcrudapplication.web.rest;
+import java.net.URISyntaxException;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import com.luan.java8defaultcrudapplication.service.ContactService;
+import com.luan.java8defaultcrudapplication.service.dto.ContactDTO;
 
 import io.swagger.annotations.Api;
 
 @RestController
-@Api("/contact/")
+@Api("/contact")
 public class ContactResource {
-
+    
+private final ContactService contactService;
+    
+	private ContactResource(ContactService contactService) {
+		this.contactService = contactService;
+	}
+	
+	@PostMapping()
+	public ResponseEntity<ContactDTO> save(@RequestBody ContactDTO contactDTO) throws URISyntaxException {
+		return ResponseEntity.ok().body(contactService.save(contactDTO));
+	}
 }
