d2a218ca41b07f76862f0e2137e017a6297a4acc
Luan Sousa <luan26@uninove.edu.br>
2018-07-29 19:21:56 -0300

    Alteration

--- src/main/java/com/luan/java8defaultcrudapplication/web/rest/ClientResource.java
+++ src/main/java/com/luan/java8defaultcrudapplication/web/rest/ClientResource.java
@@ -11,10 +11,27 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.luan.java8defaultcrudapplication.service.ClientService;
+import com.luan.java8defaultcrudapplication.service.dto.ClientDTO;
+
 
 @RestController
 @RequestMapping("/clients")
 public class ClientResource {
 
+	private final ClientService clientService;
 	
+	private ClientResource(ClientService clientService) {
+		this.clientService = clientService;
+	}
+	
+	@PostMapping()
+	public ResponseEntity<ClientDTO> save(@RequestBody ClientDTO clientDTO) throws URISyntaxException {
+		return ResponseEntity.ok().body(this.clientService.save(clientDTO));
+	}
+	
+	@PutMapping()
+	public ResponseEntity<ClientDTO> update(@RequestBody ClientDTO clientDTO) throws URISyntaxException{
+		return ResponseEntity.ok().body(this.clientService.update(clientDTO));
+	}
 }
