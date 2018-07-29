3ce95809368a669c88e7a91e132e7559c7a370e2
Luan Sousa <luan26@uninove.edu.br>
2018-07-29 19:17:37 -0300

    Client service created

---
+++ src/main/java/com/luan/java8defaultcrudapplication/service/ClientService.java
@@ -0,0 +1,19 @@
+package com.luan.java8defaultcrudapplication.service;
+
+import java.util.List;
+
+import com.luan.java8defaultcrudapplication.service.dto.ClientDTO;
+
+public interface ClientService {
+
+	ClientDTO save(ClientDTO clientDTO);
+	
+	ClientDTO update(ClientDTO clientDTO);
+	
+	List<ClientDTO> findAll();
+	
+	ClientDTO findById(Long clientId);
+	
+	void delete(Long clientId);
+	
+}
--- src/main/java/com/luan/java8defaultcrudapplication/web/rest/ClientResource.java
+++ src/main/java/com/luan/java8defaultcrudapplication/web/rest/ClientResource.java
@@ -1,5 +1,20 @@
 package com.luan.java8defaultcrudapplication.web.rest;
+import java.net.URISyntaxException;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
+@RequestMapping("/clients")
 public class ClientResource {
 
+	
 }
