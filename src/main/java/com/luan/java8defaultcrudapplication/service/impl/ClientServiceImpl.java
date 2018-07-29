f08744e6e5ca8643b837850f43a13356503d61f2
Luan Sousa <luan26@uninove.edu.br>
2018-07-29 19:51:27 -0300

    Client ServiceImpl

---
+++ src/main/java/com/luan/java8defaultcrudapplication/service/impl/ClientServiceImpl.java
@@ -0,0 +1,42 @@
+package com.luan.java8defaultcrudapplication.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.luan.java8defaultcrudapplication.service.ClientService;
+import com.luan.java8defaultcrudapplication.service.dto.ClientDTO;
+
+@Service
+public class ClientServiceImpl implements ClientService {
+
+	@Override
+	public ClientDTO save(ClientDTO clientDTO) {
+		return null;
+	}
+
+	@Override
+	public ClientDTO update(ClientDTO clientDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ClientDTO> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClientDTO findById(Long clientId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Long clientId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
