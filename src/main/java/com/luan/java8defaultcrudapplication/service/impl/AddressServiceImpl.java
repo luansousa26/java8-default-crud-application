9919b053b128cba9f105bd3afd6960469098928e
Luan Sousa <luan26@uninove.edu.br>
2018-08-01 20:21:39 -0300

    AddressService implement created

---
+++ src/main/java/com/luan/java8defaultcrudapplication/service/impl/AddressServiceImpl.java
@@ -0,0 +1,45 @@
+package com.luan.java8defaultcrudapplication.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import com.luan.java8defaultcrudapplication.service.AddressService;
+import com.luan.java8defaultcrudapplication.service.dto.AddressDTO;
+
+@Service
+@Component
+public class AddressServiceImpl implements AddressService {
+
+	@Override
+	public AddressDTO save(AddressDTO addressDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AddressDTO update(AddressDTO addressDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<AddressDTO> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AddressDTO findById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
