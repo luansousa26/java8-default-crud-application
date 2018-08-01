a84e615abb31bc13fe7ef7457c1a78a31c6385f1
Luan Sousa <luan26@uninove.edu.br>
2018-08-01 20:09:04 -0300

    AddressDTO implemented

--- src/main/java/com/luan/java8defaultcrudapplication/service/dto/AddressDTO.java
+++ src/main/java/com/luan/java8defaultcrudapplication/service/dto/AddressDTO.java
@@ -1,5 +1,107 @@
 package com.luan.java8defaultcrudapplication.service.dto;
 
+import java.time.LocalDate;
+
 public class AddressDTO {
+	
+	private Long id;
+	
+	private String street;
+	
+	private String district;
+
+	private Long number;
+	
+	private String city;
+	
+	private String state;
+	
+	private String country;
+	
+	private String cep;
+	
+	private Long idClient;
+	
+	private LocalDate alterationDate;
+	
+	public Long geId() {
+		return this.id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getStreet() {
+		return this.street;
+	}
+	
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	
+	public String getDistrict() {
+		return this.district;
+	}
+	
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+	
+	public Long getNumber() {
+		return this.number;
+	}
+	
+	public void setNumber(Long number) {
+		this.number = number;
+	}
+	
+	public String getCity() {
+		return this.city;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public String getState() {
+		return this.state;
+	}
+	
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	public String getCountry() {
+		return this.country;
+	}
+	
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	public String getCep() {
+		return this.cep;
+	}
+	
+	public void setCep(String cep) {
+		this.cep  = cep;
+	}
+	
+	public Long geIdClient() {
+		return this.idClient;
+	}
+	
+	public void setIdClient(Long idClient) {
+		this.idClient = idClient;
+	}
+	
+	public LocalDate getAlterationDate() {
+		return this.alterationDate;
+	}
+	
+	public void setAlterationDate(LocalDate alterationDate) {
+		this.alterationDate = alterationDate;
+	}
 
 }
