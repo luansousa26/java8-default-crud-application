fdc5325b1d72f3bb5d987be14097f5e14dfa9ed1
Luan Sousa <luan26@uninove.edu.br>
2018-08-01 20:17:31 -0300

    Address service created

--- src/main/java/com/luan/java8defaultcrudapplication/service/AddressService.java
+++ src/main/java/com/luan/java8defaultcrudapplication/service/AddressService.java
@@ -1,5 +1,14 @@
 package com.luan.java8defaultcrudapplication.service;
 
+import java.util.List;
+
+import com.luan.java8defaultcrudapplication.service.dto.AddressDTO;
+
 public interface AddressService {
 
+	AddressDTO save(AddressDTO addressDTO);
+	
+	AddressDTO update(AddressDTO addressDTO);
+	
+	List<AddressDTO> findAll();
 }
