9eb21e8a69f36e90cbd1fe3ccb4c6ea024d8b489
Luan Sousa <luan26@uninove.edu.br>
2018-08-01 20:12:45 -0300

    AddressMapper created

---
+++ src/main/java/com/luan/java8defaultcrudapplication/service/mapper/AddressMapper.java
@@ -0,0 +1,26 @@
+package com.luan.java8defaultcrudapplication.service.mapper;
+
+import java.util.Optional;
+import org.mapstruct.Mapper;
+
+import com.luan.java8defaultcrudapplication.domain.Address;
+import com.luan.java8defaultcrudapplication.service.dto.AddressDTO;
+@Mapper(componentModel = "spring")
+public interface AddressMapper extends EntityMapper<AddressDTO, Address> {
+	
+		
+	AddressDTO toDto(Address address);
+	Address toEntity(AddressDTO addressDTO);
+	AddressDTO toDto(Optional<Address> address);
+	Address toEntity(Optional<AddressDTO> addressDTO);
+
+		default Address fromId(Long id) {
+	        if (id == null) {
+	            return null;
+	        }
+	        Address address = new Address();
+	        address.setId(id);
+			return address;
+		}
+
+}
