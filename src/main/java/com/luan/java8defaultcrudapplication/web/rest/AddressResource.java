b0341d0a6e7e248378f228e200b2c81ee4ade84b
Luan Sousa <luan26@uninove.edu.br>
2018-08-02 18:17:50 -0300

    Save in resource created

--- src/main/java/com/luan/java8defaultcrudapplication/web/rest/AddressResource.java
+++ src/main/java/com/luan/java8defaultcrudapplication/web/rest/AddressResource.java
@@ -1,5 +1,23 @@
 package com.luan.java8defaultcrudapplication.web.rest;
 
-public class AddressResource {
+import java.net.URISyntaxException;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luan.java8defaultcrudapplication.service.AddressService;
+import com.luan.java8defaultcrudapplication.service.dto.AddressDTO;
+
+@RestController
+@RequestMapping("/address")
+public class AddressResource {
+	
+	private AddressService addressService;
+
+	
+	public ResponseEntity<AddressDTO> save(@RequestBody AddressDTO addressDTO) throws URISyntaxException {
+		return ResponseEntity.ok().body(addressService.save(addressDTO));
+	}
 }
