aa61479ae49bb5cd550cfc27b14c9c8369536ad9
Luan Sousa <luan26@uninove.edu.br>
2018-08-02 18:18:19 -0300

    PostMapping annotation added

--- src/main/java/com/luan/java8defaultcrudapplication/web/rest/AddressResource.java
+++ src/main/java/com/luan/java8defaultcrudapplication/web/rest/AddressResource.java
@@ -3,6 +3,7 @@ package com.luan.java8defaultcrudapplication.web.rest;
 import java.net.URISyntaxException;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,7 @@ public class AddressResource {
 	
 	private AddressService addressService;
 
-	
+    @PostMapping()	
 	public ResponseEntity<AddressDTO> save(@RequestBody AddressDTO addressDTO) throws URISyntaxException {
 		return ResponseEntity.ok().body(addressService.save(addressDTO));
 	}
