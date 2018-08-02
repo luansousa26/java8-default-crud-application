ffa33bb576643c1467fd36d85ae5b64595561033
Luan Sousa <luan26@uninove.edu.br>
2018-08-02 18:29:22 -0300

    userService altered

--- src/main/java/com/luan/java8defaultcrudapplication/service/impl/UserServiceImpl.java
+++ src/main/java/com/luan/java8defaultcrudapplication/service/impl/UserServiceImpl.java
@@ -4,14 +4,11 @@ import java.time.LocalDate;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-
-import com.luan.java8defaultcrudapplication.Java8DefaultCrudApplication;
 import com.luan.java8defaultcrudapplication.repository.UserRepository;
 import com.luan.java8defaultcrudapplication.service.UserService;
 import com.luan.java8defaultcrudapplication.service.dto.UserDTO;
@@ -21,7 +18,7 @@ import com.luan.java8defaultcrudapplication.service.mapper.UserMapper;
 @Component
 public class UserServiceImpl implements UserService{
 	
-	private static final Logger log = LoggerFactory.getLogger(Java8DefaultCrudApplication.class);
+	private static final Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
 	@Autowired
 	UserRepository userRepository;
 
